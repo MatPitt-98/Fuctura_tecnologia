1208afb711c1a552b2f112e5588e84cdc19f0407
MatPitt-98 <matheusrpitt@hotmail.com>
2025-11-18 16:11:46 -0300

    Projetos Piggmeu e Rastreador de leitura movidos para repositórios dedicados

--- Python-Módulo_2/Projeto_Rastreador_de_leitura/rastreador_de_leitura/app/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Livro
-
-
-class LivroForm(forms.ModelForm):
-    class Meta:
-        model = Livro
-        fields = ['nome', 'escritor', 'editora', 'progresso', 'status', 'capa']
